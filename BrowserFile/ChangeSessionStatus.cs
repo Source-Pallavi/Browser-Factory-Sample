7ffcbd0d27c26a78046babce83fb6d3e9d205d39
Source-Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-07-25 16:41:19 +0530

    Add project files.

---
+++ BrowserFile/ChangeSessionStatus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserFile
+{
+    class ChangeSessionStatus
+    {
+        static void Main(string[] args)
+        {
+            string reqString = "{\"status\":\"completed\", \"reason\":\"\"}";
+
+            byte[] requestData = Encoding.UTF8.GetBytes(reqString);
+            Uri myUri = new Uri(string.Format("https://www.browserstack.com/automate/sessions/<session-id>.json"));
+            WebRequest myWebRequest = HttpWebRequest.Create(myUri);
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)myWebRequest;
+            myWebRequest.ContentType = "application/json";
+            myWebRequest.Method = "PUT";
+            myWebRequest.ContentLength = requestData.Length;
+            using (Stream st = myWebRequest.GetRequestStream()) st.Write(requestData, 0, requestData.Length);
+
+            NetworkCredential myNetworkCredential = new NetworkCredential("pallavidubey1", "EYS2N9avp4xvnPsHxvqw");
+            CredentialCache myCredentialCache = new CredentialCache();
+            myCredentialCache.Add(myUri, "Basic", myNetworkCredential);
+            myHttpWebRequest.PreAuthenticate = true;
+            myHttpWebRequest.Credentials = myCredentialCache;
+
+            myWebRequest.GetResponse().Close();
+        }
+    }
+}
+
